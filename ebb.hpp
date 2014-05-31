5caf04b01daeb2a4918286aebec07e9936e2d367
Igor Kaplounenko <igor@bittorrent.com>
2014-05-30 17:02:56 -0700

    amazing fun with the preprocessor

--- ebb.hpp
+++ ebb.hpp
@@ -8,10 +8,35 @@
 #include <cinttypes>
 #include <cstdint>
 #include <cstdio>
+#include <cstdlib>
 #include <cstring>
 #include <string>
 #include <tuple>
 #include <vector>
+
+#if defined(BOOST_PP_VARIADICS)
+#if !BOOST_PP_VARIADICS
+#error BOOST_PP_VARIADICS cannot be defined as 0 when using Ebb
+#endif
+#else
+#define BOOST_PP_VARIADICS 1
+#endif 
+#include <boost/preprocessor/arithmetic/dec.hpp>
+#include <boost/preprocessor/comparison/not_equal.hpp>
+#include <boost/preprocessor/control/iif.hpp>
+#include <boost/preprocessor/facilities/empty.hpp>
+#include <boost/preprocessor/punctuation/comma_if.hpp>
+#include <boost/preprocessor/seq/cat.hpp>
+#include <boost/preprocessor/seq/first_n.hpp>
+#include <boost/preprocessor/seq/for_each.hpp>
+#include <boost/preprocessor/seq/for_each_i.hpp>
+#include <boost/preprocessor/seq/seq.hpp>
+#include <boost/preprocessor/seq/to_tuple.hpp>
+#include <boost/preprocessor/stringize.hpp>
+#include <boost/preprocessor/tuple/elem.hpp>
+#include <boost/preprocessor/tuple/enum.hpp>
+#include <boost/preprocessor/tuple/size.hpp>
+#include <boost/preprocessor/tuple/to_seq.hpp>
 
 #ifndef PRId64
 #if defined MSC_VER || defined __MINGW32_
@@ -29,10 +54,36 @@ namespace ebb {
 		// generated sequence specialization / endpoint
 		template<int... S> struct gen_seq<0, S...> { typedef seq<S...> type; };
 
+		// for enforcing lexicografic key order
+		constexpr bool is_less_than(char const* left, char const* right) {
+			return (*left < *right ||
+					(*left == *right && *left && is_less_than(left+1, right+1)));
+		}
+
+		constexpr bool is_valid_key_order(char const* remaining) { return true; }
+
+		template<typename... T>
+		constexpr bool is_valid_key_order(char const* left, char const* right,
+				T... remaining) {
+			return is_less_than(left, right) ?
+				is_valid_key_order(right, remaining...) : false;
+		}
+
 		// for list and dict start/end tokens, i.e. 'd', 'l', 'e'
 		struct bencode_token {
 			const unsigned char token;
 		}; 
+
+		struct bencoded_dict {
+			unsigned char const* bdecode(unsigned char const* buffer, size_t &len) {
+				assert(false);
+				return NULL;
+			}
+			unsigned char* bencode(unsigned char* buffer, size_t len) {
+				assert(false);
+				return NULL;
+			}
+		};
 
 
 		template<typename> struct is_unsigned_char_array : std::false_type {};
@@ -105,19 +156,6 @@ namespace ebb {
 				return buffer;
 			}
 
-			template<typename... Arguments> unsigned char* bencode(std::int64_t value,
-					Arguments&&... remaining) {
-				long written = snprintf(reinterpret_cast<char*>(buffer), len,
-						"i%" PRId64 "e", value);
-				if (written > len) {
-					buffer = NULL;
-					return NULL;
-				}
-				buffer += written;
-				len -= written;
-				return bencode(remaining...);
-			}
-
 			template<typename... Arguments> unsigned char* bencode(char const *value,
 					Arguments&&... remaining) {
 				long written = snprintf(reinterpret_cast<char*>(buffer), len,
@@ -146,8 +184,36 @@ namespace ebb {
 				return bencode_listish(value, remaining...);
 			}
 
+			template<typename T, typename... Arguments> typename
+					std::enable_if<std::is_integral<T>::value, unsigned char*>::type
+					bencode(T value, Arguments&&... remaining) {
+				long written = snprintf(reinterpret_cast<char*>(buffer), len,
+						"i%" PRId64 "e", std::int64_t(value));
+				if (written > len) {
+					buffer = NULL;
+					return NULL;
+				}
+				buffer += written;
+				len -= written;
+				return bencode(remaining...);
+			}
+
+			template<typename T, typename... Arguments> typename std::enable_if<
+					std::is_base_of<detail::bencoded_dict, T>::value,
+					unsigned char*>::type bencode(T const &value,
+							Arguments&&... remaining) {
+				unsigned char* result = value.bencode(buffer, len);
+				if (result == NULL) {
+					buffer = NULL;
+					return NULL;
+				}
+				len -= result - buffer;
+				buffer = result;
+				return bencode(remaining...);
+			}
+			
 			template<size_t N, typename... Arguments> unsigned char* bencode(
-					std::array<const unsigned char, N> const &value,
+					std::array<unsigned char const, N> const &value,
 					Arguments&&... remaining) {
 				return bencode_listish(value, remaining...);
 			}
@@ -181,7 +247,7 @@ namespace ebb {
 						std::tuple<TupleTypes...> const &value, Arguments... remaining) {
 				return bencode(std::get<S>(value)..., remaining...);
 			}
-			
+
 			template<typename T, typename... Arguments>
 			unsigned char* bencode_listish(T const& value, Arguments&&... remaining) {
 				if (value.size() > INT64_MAX) {
@@ -199,5 +265,153 @@ namespace ebb {
 				len -= written + value.size();
 				return bencode(remaining...);
 			}
+
+			//TODO: array to tuple, perhaps?
+			template<typename T, size_t S, typename... Arguments> unsigned char*
+				bencode(std::array<T, S> const& value, Arguments&&... remaining) {
+				bencode(blist_begin);
+				for(auto const& v : value) {
+					bencode(v);
+				}
+				return bencode(blist_end, remaining...);
+			}
+
+			template<typename T, typename... Arguments> unsigned char*
+				bencode(std::vector<T> const& value, Arguments&&... remaining) {
+				bencode(blist_begin);
+				for(auto const& v : value) {
+					bencode(v);
+				}
+				return bencode(blist_end, remaining...);
+			}
 	};
+
+	inline unsigned char const* bdecode(unsigned char const* buffer, size_t &len,
+			std::int64_t &value) {
+		// int has to start with 'i' and followed by at least one digit and 'e'
+		if (len < 3 || buffer[0] != 'i') {
+			return NULL;
+		}
+		unsigned char* end;
+		static_assert(sizeof(long long) == sizeof(std::int64_t),
+				"long long must be int64_t");
+		value = std::strtoll(reinterpret_cast<char const*>(buffer + 1),
+				reinterpret_cast<char**>(&end), 10);
+		// we read nothing, or we read past the end of the buffer, or int is not
+		// terminated by 'e'
+		if (buffer + 1 == end || buffer + len <= end || *end != 'e') {
+			return NULL;
+		}
+		len -= (end - buffer + 1);
+		return end + 1;
+	}
+
+	inline unsigned char const* bdecode(unsigned char const* buffer, size_t &len,
+			std::string &value) {
+		if (len < 2) {
+			buffer = NULL;
+			return NULL;
+		}
+		unsigned char* end;
+		std::int64_t value_len = std::strtoll(reinterpret_cast<char const*>(buffer),
+				reinterpret_cast<char **>(&end), 10);
+		// we read nothing, or our string has a negative length, or the string
+		// extends past the end of the buffer
+		if (buffer == end || value_len < 0 || buffer + len < end + value_len + 1) {
+			return NULL;
+		}
+		value.resize(value_len);
+		std::copy(end, end + value_len, value.begin());
+		len -= (end + value_len + 1 - buffer);
+		return end + value_len;
+	}
+
+	template<typename T> unsigned char const* bdecode(
+			unsigned char const* buffer, size_t &len, std::vector<T> &value) {
+		if (len < 2 || *buffer != 'l') {
+			return NULL;
+		}
+		buffer++;
+		value.clear();
+		while(*buffer != 'e') {
+			T element;
+			buffer = bdecode(buffer, len, element);
+			if (buffer == NULL) {
+				value.clear();
+				return NULL;
+			}
+			value.push_back(element);
+		}
+		len++;
+		return buffer + 1;
+	}
 }
+
+// here be dragons
+// long story short, this wraps boost sequence elements in an extra set of
+// parentheses, turning each one into a tuple
+#define EBB_ADD_PAREN_1(...) ((__VA_ARGS__)) EBB_ADD_PAREN_2
+#define EBB_ADD_PAREN_2(...) ((__VA_ARGS__)) EBB_ADD_PAREN_1
+#define EBB_ADD_PAREN_1_END
+#define EBB_ADD_PAREN_2_END
+#define EBB_PARENIFY(A) BOOST_PP_CAT(EBB_ADD_PAREN_1 A,_END)
+
+#define EBB_GET_ARG_TYPE(TUPLE) \
+	BOOST_PP_TUPLE_ENUM(BOOST_PP_SEQ_TO_TUPLE(BOOST_PP_SEQ_FIRST_N(BOOST_PP_DEC( \
+					BOOST_PP_TUPLE_SIZE(TUPLE)), BOOST_PP_TUPLE_TO_SEQ(TUPLE))))
+#define EBB_GET_ARG_NAME(TUPLE) \
+	BOOST_PP_TUPLE_ELEM(BOOST_PP_DEC(BOOST_PP_TUPLE_SIZE(TUPLE)), TUPLE)
+
+#define EBB_ENCODE_ARG(R, DATA, I, TUPLE) \
+	BOOST_PP_COMMA_IF(I) BOOST_PP_STRINGIZE(EBB_GET_ARG_NAME(TUPLE)) \
+	BOOST_PP_COMMA() this->EBB_GET_ARG_NAME(TUPLE)
+
+#define EBB_STRINGIFY_ARG(R, DATA, I, TUPLE) \
+	BOOST_PP_COMMA_IF(I) BOOST_PP_STRINGIZE(EBB_GET_ARG_NAME(TUPLE))
+
+#define EBB_ATTR_DECL(R, DATA, TUPLE) \
+	EBB_GET_ARG_TYPE(TUPLE) EBB_GET_ARG_NAME(TUPLE);
+
+#define EBB_MAKE_BENCODED_ATTRIBUTES(ATTRIBUTES) \
+	BOOST_PP_SEQ_FOR_EACH(EBB_ATTR_DECL, 0, EBB_PARENIFY(ATTRIBUTES))
+
+#define EBB_MAKE_ARGS(ARGS) \
+	BOOST_PP_SEQ_FOR_EACH_I(EBB_ENCODE_ARG, 0, EBB_PARENIFY(ARGS))
+
+#define EBB_VALIDATE_ARGS(ARGS) \
+	static_assert(ebb::detail::is_valid_key_order( \
+				BOOST_PP_SEQ_FOR_EACH_I(EBB_STRINGIFY_ARG, 0, EBB_PARENIFY(ARGS))), \
+			"dictionary keys are not lexicographically sorted");
+
+#define EBB_MAKE_BENCODE(ATTRIBUTES) \
+	unsigned char* bencode(unsigned char* buffer, size_t len) const { \
+		EBB_VALIDATE_ARGS(ATTRIBUTES) \
+		return ebb::bencoder(buffer, len)(ebb::bdict_begin, \
+				EBB_MAKE_ARGS(ATTRIBUTES), ebb::bdict_end); \
+	} \
+	\
+	template <size_t Size> \
+	unsigned char* bencode(std::array<unsigned char, Size> &buffer) const { \
+		return this->bencode(buffer.data(), buffer.size()); \
+	}
+
+#define EBB_MAKE_BDECODE(ATTRIBUTES) \
+	unsigned char const* bdecode(unsigned char const* buffer, size_t &len) { \
+		return NULL; \
+	}
+
+/*	e.g.
+ *
+ *  EBB_MAKE_BENCODED_DICT(my_dict,
+ *  		(int, a)(char const*, b)(std::array<unsigned char, 5>, c))
+ *
+ *  will create a dictionary { 'a': a, 'b' : b, 'c': c }
+ */
+#define EBB_MAKE_BENCODED_DICT(NAME, ATTRIBUTES) \
+struct NAME : ebb::detail::bencoded_dict { \
+	EBB_MAKE_BENCODED_ATTRIBUTES(ATTRIBUTES) \
+	\
+	EBB_MAKE_BENCODE(ATTRIBUTES) \
+	\
+	EBB_MAKE_BDECODE(ATTRIBUTES) \
+};
--- tests/TestBencoder.cpp
+++ tests/TestBencoder.cpp
@@ -21,7 +21,17 @@ inline int snprintf(char* buf, int len, char const* fmt, ...)
 
 using namespace ebb;
 
-TEST(ebb, integer) {
+EBB_MAKE_BENCODED_DICT(benc_dict,
+		(std::array<unsigned char, 1>, array)
+		(int, integer)
+		(std::int64_t, integer64)
+		(std::string, string)
+		(std::vector<unsigned char>, vector))
+
+EBB_MAKE_BENCODED_DICT(benc_dict_nested,
+		(std::array<benc_dict, 2>, dicts))
+
+TEST(bencode, integer) {
 	unsigned char output[1024];
 	unsigned char* last = bencoder(output, 1024)(
 			3000ll
@@ -31,7 +41,7 @@ TEST(ebb, integer) {
 	EXPECT_STREQ("i3000e", reinterpret_cast<char*>(output));
 }
 
-TEST(ebb, integer_bounds) {
+TEST(bencode, integer_bounds) {
 	unsigned char output[4];
 	unsigned char* last = bencoder(output, 4)(
 			3000ll
@@ -39,7 +49,7 @@ TEST(ebb, integer_bounds) {
 	EXPECT_EQ(static_cast<unsigned char*>(NULL), last);
 }
 
-TEST(ebb, string) {
+TEST(bencode, string) {
 	unsigned char output[1024];
 	unsigned char* last = bencoder(output, 1024)(
 			"asdf"
@@ -49,7 +59,7 @@ TEST(ebb, string) {
 	EXPECT_STREQ("4:asdf", reinterpret_cast<char*>(output));
 }
 
-TEST(ebb, string_bounds) {
+TEST(bencode, string_bounds) {
 	unsigned char output[4];
 	unsigned char* last = bencoder(output, 4)(
 			"asdf"
@@ -57,7 +67,7 @@ TEST(ebb, string_bounds) {
 	EXPECT_EQ(static_cast<unsigned char*>(NULL), last);
 }
 
-TEST(ebb, array) {
+TEST(bencode, array) {
 	unsigned char output[1024];
 	std::array<unsigned char, 3> data1 = {{'\1', '\0', '\2'}};
 	unsigned char* last = bencoder(output, 1024)(
@@ -67,7 +77,7 @@ TEST(ebb, array) {
 	EXPECT_EQ(0, memcmp("3:\x01\x00\x02", output, 3));
 }
 
-TEST(ebb, const_array) {
+TEST(bencode, const_array) {
 	unsigned char output[1024];
 	std::array<const unsigned char, 3> data1 = {{'\1', '\0', '\2'}};
 	unsigned char* last = bencoder(output, 1024)(
@@ -77,7 +87,7 @@ TEST(ebb, const_array) {
 	EXPECT_EQ(0, memcmp("3:\x01\x00\x02", output, 3));
 }
 
-TEST(ebb, array_bounds) {
+TEST(bencode, array_bounds) {
 	unsigned char output[4];
 	std::array<unsigned char, 3> data1 = {{'\1', '\0', '\2'}};
 	unsigned char* last = bencoder(output, 4)(
@@ -86,7 +96,7 @@ TEST(ebb, array_bounds) {
 	EXPECT_EQ(static_cast<unsigned char*>(NULL), last);
 }
 
-TEST(ebb, vector) {
+TEST(bencode, vector) {
 	unsigned char output[1024];
 	std::vector<unsigned char> data1 = {{'\1', '\0', '\2'}};
 	unsigned char* last = bencoder(output, 1024)(
@@ -96,7 +106,7 @@ TEST(ebb, vector) {
 	EXPECT_EQ(0, memcmp("3:\x01\x00\x02", output, 3));
 }
 
-TEST(ebb, vector_bounds) {
+TEST(bencode, vector_bounds) {
 	unsigned char output[4];
 	std::vector<unsigned char> data1 = {{'\1', '\0', '\2'}};
 	unsigned char* last = bencoder(output, 4)(
@@ -105,7 +115,7 @@ TEST(ebb, vector_bounds) {
 	EXPECT_EQ(static_cast<unsigned char*>(NULL), last);
 }
 
-TEST(ebb, list) {
+TEST(bencode, list) {
 	unsigned char output[1024];
 	unsigned char* last = bencoder(output, 1024)(
 			blist(
@@ -117,7 +127,7 @@ TEST(ebb, list) {
 	EXPECT_STREQ("l1:a1:b1:c1:de", reinterpret_cast<const char*>(output));
 }
 
-TEST(ebb, dict) {
+TEST(bencode, dict) {
 	unsigned char output[1024];
 	unsigned char* last = bencoder(output, 1024)(
 			bdict(
@@ -130,7 +140,7 @@ TEST(ebb, dict) {
 	EXPECT_STREQ("d1:1i2e1:31:4e", reinterpret_cast<const char*>(output));
 }
 
-TEST(ebb, key_types) {
+TEST(bencode, key_types) {
 	unsigned char output[1024];
 	const char* data0 = "abc";
 	char* data0_1 = const_cast<char*>(data0);
@@ -151,7 +161,7 @@ TEST(ebb, key_types) {
 	EXPECT_NE(static_cast<unsigned char*>(NULL), last);
 }
 
-TEST(ebb, multiple_writes) {
+TEST(bencode, multiple_writes) {
 	unsigned char output[1024];
 	const char* data0 = "abc";
 	const char* data1 = "def";
@@ -163,7 +173,7 @@ TEST(ebb, multiple_writes) {
 	EXPECT_STREQ("l3:abc3:defe", reinterpret_cast<const char*>(output));
 }
 
-TEST(ebb, fancy) {
+TEST(bencode, fancy) {
 	unsigned char output[1024];
 	std::array<unsigned char, 3> data1 = {{'e', 'f', 'g'}};
 	std::vector<unsigned char> data2 = {{'h', 'i', 'j'}};
@@ -180,7 +190,7 @@ TEST(ebb, fancy) {
 	EXPECT_STREQ(expected, reinterpret_cast<const char*>(output));
 }
 
-TEST(ebb, array_constructor) {
+TEST(bencode, array_constructor) {
 	std::array<unsigned char, 1024> output;
 	std::array<unsigned char, 3> data1 = {{'e', 'f', 'g'}};
 	std::vector<unsigned char> data2 = {{'h', 'i', 'j'}};
@@ -192,6 +202,46 @@ TEST(ebb, array_constructor) {
 				k_v("list", blist("a", "b", "c", "d", data1, data2))
 				)
 			);
+	EXPECT_NE(static_cast<unsigned char*>(NULL), last);
+	*last = '\0';
+	EXPECT_STREQ(expected, reinterpret_cast<const char*>(output.data()));
+}
+
+TEST(bencode, bencoded_dict) {
+	benc_dict d;
+	d.integer = 1;
+	d.integer64 = 2;
+	d.string = "3";
+	d.array[0] = '4';
+	d.vector.push_back('5');
+
+	const char* expected = "d7:integeri1e9:integer64i2e6:string1:35:array1:4"
+		"6:vector1:5e";
+
+	std::array<unsigned char, 1024> output;
+	unsigned char* last = d.bencode(output);
+	EXPECT_NE(static_cast<unsigned char*>(NULL), last);
+	*last = '\0';
+	EXPECT_STREQ(expected, reinterpret_cast<const char*>(output.data()));
+}
+
+TEST(bencode, bencoded_dict_nested) {
+	benc_dict_nested d;
+	for (auto& dict : d.dicts) {
+		dict.integer = 1;
+		dict.integer64 = 2;
+		dict.string = "3";
+		dict.array[0] = '4';
+		dict.vector.push_back('5');
+	}
+
+	const char* expected = "d5:dictsl"
+		"d7:integeri1e9:integer64i2e6:string1:35:array1:46:vector1:5e"
+		"d7:integeri1e9:integer64i2e6:string1:35:array1:46:vector1:5e"
+		"ee";
+
+	std::array<unsigned char, 1024> output;
+	unsigned char* last = d.bencode(output);
 	EXPECT_NE(static_cast<unsigned char*>(NULL), last);
 	*last = '\0';
 	EXPECT_STREQ(expected, reinterpret_cast<const char*>(output.data()));
